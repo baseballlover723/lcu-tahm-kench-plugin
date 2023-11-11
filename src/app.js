542f2acc6ea056513dac6f89cf9ad475a45a8ed9
Philip Ross <dev@baseballlover723.com>
2023-11-10 19:04:54 -0800

    connects to league

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-console.log("running");
+const LCUConnector = require('lcu-connector');
+
+const connector = new LCUConnector();
+connector.on('connect', (data) => {
+  connector.on('connect', data => {
+    console.log('League Client has started', data);
+  });
+});
+
+connector.on('disconnect', () => {
+  console.log('League Client has been closed');
+});
+
+// Start listening for the LCU client
+connector.start();
+console.log('Listening for League Client');
